1cedefa3338b2bafc920461878aa958781e92d2e
Josh Date <date0010@me.com>
2015-04-26 22:32:05 -0700

    added rough draft about me text, fine tuned some colors and added a works page outline

---
+++ scripts/58849175.main.js
@@ -0,0 +1,1 @@
+"use strict";$(".work").click(function(){$(".full-window").animate({marginLeft:"25%"},300,function(){$(".full-window").animate({marginLeft:"-100%"},500,function(){window.location.href="works.html"})})});

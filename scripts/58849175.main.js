fabc55b215e9860759b116eb6d6816e42943c427
Josh Date <date0010@me.com>
2015-05-25 21:30:36 -0700

    updated stop light buttons to function. added easter egg picture

--- scripts/58849175.main.js
+++
@@ -1,1 +0,0 @@
-"use strict";$(".work").click(function(){$(".full-window").animate({marginLeft:"25%"},300,function(){$(".full-window").animate({marginLeft:"-100%"},500,function(){window.location.href="works.html"})})});
